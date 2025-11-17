0b5a83fb2f80d44bc7b35a37a2945b1cf47a6c99
Abdul Hadi <www.abdulhadi2005@gmail.com>
2025-11-17 11:51:03 +0500

    Add files via upload

---
+++ Lab_13/L13_T1_65567.asm
@@ -0,0 +1,47 @@
+.model small
+.stack 100h
+
+.data
+Name_msg db 'Abdul Hadi$'
+SapID_msg db '65567$'
+Course_msg db 'COAL$'
+
+.code 
+
+newline proc
+    mov dl,10
+    mov ah,2
+    int 21h
+    
+    mov dl,13
+    mov ah,2
+    int 21h
+    
+    ret
+newline endp
+        
+main proc
+    mov ax, @data
+    mov ds, ax
+    
+    mov ah,09
+    mov dx, offset Name_msg
+    int 21h
+    
+    call newline
+    mov ah,09
+    mov dx, offset SapID_msg
+    int 21h
+    
+    call newline
+    mov ah,09
+    mov dx, offset Course_msg
+    int 21h   
+    
+    mov ah,4ch
+    int 21h
+
+main endp         
+end main         
+
+
